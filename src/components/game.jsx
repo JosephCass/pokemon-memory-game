6e7cb7bc7d9c9a647da430d195a627c1d8a8ca0f
JosephCass <josephcassinera@gmail.com>
2022-10-27 16:44:25 -0400

    Changed how I render the game button elements
    The elements are now created by looping through an array of objects that are meant to keep track of the state of the elements when they are clicked.

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,78 +1,118 @@
+import { useRef } from "react";
 import { useState } from "react";
 
 export default function Game() {
   let [level, setLevel] = useState(0);
 
+  let [lostGame, setLostState] = useState(false);
+
   let levelDifficulty = [4, 6, 8, 10, 12];
 
-  function validateChoice() {
-    //check if right
-    //check if level finished
-    //keep track of choices left
+  let buttonContainer = useRef(null);
+
+  function handleLostGame() {
+    setLostState(true);
+  }
+
+  function checkLevelComplete(e) {}
+
+  function validateChoice(key) {
+    setGameButtons(
+      gameButtons.map((gameButton) => {
+        return gameButton.key === key
+          ? { ...gameButton, clicked: true }
+          : { ...gameButton };
+      })
+    );
   }
 
   function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  function renderLevel() {
-    let gameArray = [];
-    // https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png
-    for (let x = 0; x < levelDifficulty[level]; x++) {
-      let ranNum = randomIntFromInterval(1, 905);
-      if (ranNum >= 100) {
-        gameArray.push(
-          <button className="pokemon-game-button">
+  const initialButtons = [];
+  for (let x = 0; x < levelDifficulty[level]; x++) {
+    const randNum = randomIntFromInterval(1, 905);
+    //Implement a check to see if the random number was already choosen before pushng, rare case
+    initialButtons.push({ clicked: false, key: randNum });
+  }
+
+  const [gameButtons, setGameButtons] = useState(initialButtons);
+
+  function renderGameBtns() {
+    return gameButtons.map((currBtn) => {
+      if (currBtn.key >= 100) {
+        return (
+          <button
+            data=""
+            onClick={() => validateChoice(currBtn.key)}
+            className="pokemon-game-button"
+            key={`btn-${currBtn.key}`}
+          >
             <img
-              picked=""
-              key=""
-              src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${ranNum}.png`}
+              key={`img-${currBtn.key}`}
+              src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${currBtn.key}.png`}
               alt="Pokemon picture"
               className="pokemon-button-img"
             />
           </button>
         );
-      } else if (ranNum >= 10) {
-        gameArray.push(
-          <button className="pokemon-game-button">
+      } else if (currBtn.key >= 10) {
+        return (
+          <button
+            data=""
+            onClick={() => validateChoice(currBtn.key)}
+            className="pokemon-game-button"
+            key={`btn-${currBtn.key}`}
+          >
             <img
-              picked=""
-              key=""
-              src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/0${ranNum}.png`}
+              key={`img-${currBtn.key}`}
+              src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/0${currBtn.key}.png`}
               alt="Pokemon picture"
               className="pokemon-button-img"
             />
           </button>
         );
       } else {
-        gameArray.push(
-          <button className="pokemon-game-button">
+        return (
+          <button
+            data=""
+            onClick={() => validateChoice(currBtn.key)}
+            className="pokemon-game-button"
+            key={`btn-${currBtn.key}`}
+          >
             <img
-              picked=""
-              key=""
-              src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${ranNum}.png`}
+              key={`img-${currBtn.key}`}
+              src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${currBtn.key}.png`}
               alt="Pokemon picture"
               className="pokemon-button-img"
             />
           </button>
         );
       }
-    }
-    return gameArray;
+    });
   }
 
   return (
     <div className="game-container">
-      <div className="game-heading">
-        <h3 className="game-level">{`Pick a pokemon! | Level: ${
-          level + 1
-        }`}</h3>
-        <p className="game-description">
-          If you pick the same pokemon twice you loose!
-        </p>
-      </div>
+      {!lostGame && (
+        <div className="game-heading">
+          <h3 className="game-level">{`Pick a pokemon! | Level: ${
+            level + 1
+          }`}</h3>
+          <p className="game-description">
+            If you pick the same pokemon twice you loose!
+          </p>
+        </div>
+      )}
       <div className="game-logic">
-        <div className="pokemon-btn-container">{renderLevel()}</div>
+        {!lostGame ? (
+          <div ref={buttonContainer} className="pokemon-btn-container">
+            {renderGameBtns()}
+          </div>
+        ) : (
+          <button className="play-again-btn">Play Again</button>
+        )}
       </div>
     </div>
   );
